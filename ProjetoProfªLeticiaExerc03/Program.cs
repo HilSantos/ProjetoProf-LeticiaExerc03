f3affafa5bfc420dcb7633ea0476bc4af4b8ba02
hilton.lssantos <hilton.lssantos@RIP0576973W10-1>
2024-10-04 17:09:26 -0300

    Adicionar arquivos de projeto.

---
+++ ProjetoProfªLeticiaExerc03/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoProfªLeticiaExerc03
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Double Celsius, Fahrenheit;
+
+            Console.WriteLine("Informe a Temperatura em Celsius: ");
+            Temperatura = Convert.ToDouble(Console.ReadLine());
+            Fahrenheit = (Celsius * 1.8) + 32;
+
+            Console.WriteLine("Exiba o resultado " +Celsius+ "para" +Fahrenheit);
+
+            Console.ReadKey();
+
+        }
+    }
+}
